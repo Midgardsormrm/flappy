9d212ec2355e687e51686676c6bef635c6548b23
Midgardsormrm <xsamurkin@gmail.com>
2025-05-28 00:28:20 +0300

    fix pubspec

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -3,12 +3,13 @@ import 'package:flame/flame.dart';
 import 'package:flappy/game/assets.dart';
 import 'package:flappy/game/flappy_game.dart';
 
+// ignore: deprecated_member_use
 class Background extends SpriteComponent with HasGameRef<FlappyBirdGame> {
   Background();
 
   @override
   Future<void> onLoad() async {
-    final background = await Flame.images.load('background-day.png');
+    final background = await Flame.images.load(Assets.background);
     size = gameRef.size;
     sprite = Sprite(background);
   }
